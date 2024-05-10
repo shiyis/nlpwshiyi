d73106f85b1a1201c836189df7fbb287d8836d51
shiyis <shiyis3447@gmail.com>
2024-05-10 20:50:38 +0000

    Deploying to main from  @ 3d1d5eaecabd47341b9a59d8817d73fcf5c7bfad 🚀

--- __site/youtube_videos.jl
+++
@@ -1,8 +0,0 @@
-# This dictionary maps easy to remember names to Youtube video IDs
-# after adding an ID here, you can use the {{youtube <shortname>}}
-# syntax in your markdown files to embed the video into the page!
-videos = Dict(
-              "automatic-diff" => "cNADlHfHQHg",
-              "noisy-channel" => "zjWXLD_ihOc"
-
-)
